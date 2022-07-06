ea0389555c42dcaf2b6bb62ccdbe28efa0350253
otienosteve <steveotieno701@gmail.com>
2022-07-06 12:15:42 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React,{Component } from "react";
 
-function NavBar() {
-  return (
+class  NavBar extends Component{
+  render(){return (
     <nav>
       <a href="#home">Home</a>
       <a href="#about">About</a>
     </nav>
   );
 }
-
+}
 export default NavBar;
